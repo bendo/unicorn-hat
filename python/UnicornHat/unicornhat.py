0cea89ad56d970dc262193c093aaddc8a4995acd
bendo <zdeno.osina@gmail.com>
2016-05-22 15:53:34 +0200

    PHAT from pimoroni merged

--- python/UnicornHat/unicornhat.py
+++ python/UnicornHat/unicornhat.py
@@ -3,12 +3,12 @@ import atexit
 import colorsys
 
 # LED strip configuration:
-LED_COUNT      = 64      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_COUNT = 64  # Number of LED pixels.
+LED_PIN = 18  # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 5  # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 
 ws2812 = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 ws2812.begin()
@@ -19,15 +19,17 @@ Store the rotation of UnicornHat, defaults to
 HDMI port facing downwards
 """
 _rotation = 0
-
+_wx = 8
+_wy = 8
+_map = []
 
 """
 Store a map of pixel indexes for
 translating x, y coordinates.
 """
-map = [
-    [7 , 6 , 5 , 4 , 3 , 2 , 1 , 0 ],
-    [8 , 9 , 10, 11, 12, 13, 14, 15],
+HAT = [
+    [7, 6, 5, 4, 3, 2, 1, 0],
+    [8, 9, 10, 11, 12, 13, 14, 15],
     [23, 22, 21, 20, 19, 18, 17, 16],
     [24, 25, 26, 27, 28, 29, 30, 31],
     [39, 38, 37, 36, 35, 34, 33, 32],
@@ -35,6 +37,39 @@ map = [
     [55, 54, 53, 52, 51, 50, 49, 48],
     [56, 57, 58, 59, 60, 61, 62, 63]
 ]
+
+PHAT = [
+    [24, 16, 8, 0],
+    [25, 17, 9, 1],
+    [26, 18, 10, 2],
+    [27, 19, 11, 3],
+    [28, 20, 12, 4],
+    [29, 21, 13, 5],
+    [30, 22, 14, 6],
+    [31, 23, 15, 7]
+]
+
+AUTO = None
+
+
+def set_layout(pixel_map=AUTO):
+    global _map
+    if pixel_map is None:
+        pixel_map = PHAT  # Assume PHAT
+        try:
+            product = open("/proc/device-tree/hat/product", "r").read().strip()
+            if product[:11] == "Unicorn HAT":
+                pixel_map = HAT
+        except IOError:
+            pass
+
+    _map = pixel_map
+
+
+def get_shape():
+    global _map
+
+    return len(_map), len(_map[0])
 
 
 def _clean_shutdown():
@@ -46,12 +81,12 @@ def _clean_shutdown():
 
 def rotation(r=0):
     """Set the display rotation
-
     Valid values:
     0
     90
     180
-    270"""
+    270
+    :param r: """
 
     global _rotation
     if r in [0, 90, 180, 270]:
@@ -63,21 +98,19 @@ def rotation(r=0):
 
 def brightness(b=0.2):
     """Set the display brightness between 0.0 and 1.0
-
     0.2 is highly recommended, UnicornHat can get painfully bright!"""
 
     if b > 1 or b < 0:
         raise ValueError('Brightness must be between 0.0 and 1.0')
 
-    ws2812.setBrightness(int(b*255.0))
+    ws2812.setBrightness(int(b * 255.0))
 
 
 def get_brightness():
     """Get the display brightness value
-
     Returns a float between 0.0 and 1.0
     """
-    return 0#ws2812.getBrightness()
+    return 0  # ws2812.getBrightness()
 
 
 def clear():
@@ -88,7 +121,6 @@ def clear():
 
 def off():
     """Clear the buffer and immediately update UnicornHat
-
     Turns off all pixels."""
     clear()
     show()
@@ -96,28 +128,36 @@ def off():
 
 def get_index_from_xy(x, y):
     """Convert an x, y value to an index on the display"""
-    if x > 7 or x < 0:
-        raise ValueError('X position must be between 0 and 7')
-    if y > 7 or y < 0:
-        raise ValueError('Y position must be between 0 and 7')
+    wx = len(_map) - 1
+    wy = len(_map[0]) - 1
 
-    y = 7-y
+    # if x > wx or x < 0:
+    #    raise ValueError('X position must be between 0 and {wx}'.format(wx=wx))
+    # if y > wy or y < 0:
+    #    raise ValueError('Y position must be between 0 and {wy}'.format(wy=wy))
 
-    if _rotation == 90:
-        x, y = y, 7-x
+    y = wy - y
+
+    if _rotation == 90 and wx == wy:
+        x, y = y, wx - x
     elif _rotation == 180:
-        x, y = 7-x, 7-y
-    elif _rotation == 270:
-        x, y = 7-y, x
+        x, y = wx - x, wy - y
+    elif _rotation == 270 and wx == wy:
+        x, y = wy - y, x
 
-    return map[x][y]
+    try:
+        index = _map[x][y]
+    except IndexError:
+        index = None
+
+    return index
 
 
 def set_pixel_hsv(x, y, h, s, v):
     """Set a single pixel to a colour using HSV"""
     index = get_index_from_xy(x, y)
     if index is not None:
-        r, g, b = [int(n*255) for n in colorsys.hsv_to_rgb(h, s, v)]
+        r, g, b = [int(n * 255) for n in colorsys.hsv_to_rgb(h, s, v)]
         ws2812.setPixelColorRGB(index, r, g, b)
 
 
@@ -152,5 +192,7 @@ def show():
     """Update UnicornHat with the contents of the display buffer"""
     ws2812.show()
 
+
+set_layout(HAT)
 
 atexit.register(_clean_shutdown)
